4fb8f836503ca75a0e1b607a57be7371570911ef
Dat Nguyen <dat@duo-studio.co>
2023-10-08 19:45:02 +0200

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //
+  // var tl = gsap.timeline();
+  // tl.to
+});
+
+window.addEventListener("load", function () {
+  document.body.classList.remove("loading");
+});
