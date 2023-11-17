aa328bec839f3b07a83808e52c5845ac2fe530e3
Dat Nguyen <dat@duo-studio.co>
2023-11-16 19:54:09 -0500

    qa

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  // FORM SUBMISSION (https://docs.netlify.com/forms/setup/)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    const name = myForm.getAttribute("name");
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => document.querySelector(".success").classList.add("active"))
+      .catch((error) => alert(error));
+  };
+
+  document.querySelectorAll("form").forEach((form) => {
+    form.addEventListener("submit", handleSubmit);
+  });
+});
 
 window.addEventListener("load", function () {
   document.body.classList.remove("loading");
+
+  gsap.to("#banner h1 img ", {
+    yPercent: 15,
+    scrollTrigger: {
+      trigger: "#banner",
+      start: "top top",
+      scrub: true,
+    },
+  });
 
   var toggles = document.querySelectorAll(".slider__vertical--toggle li"),
     wrapper = document.querySelector(".slider__vertical--wrapper"),
